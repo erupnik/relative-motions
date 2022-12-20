649247c20b2b76cac14b29f814c40326b5b1c42d
er <erupnik@gmail.com>
2022-12-20 13:54:59 +0100

    Add latest local devs

---
+++ build/CMakeFiles/triplet_utils.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/er/Documents/d_dev/structureless/propcov/relative-motions/src/bundle.cpp" "/home/er/Documents/d_dev/structureless/propcov/relative-motions/build/CMakeFiles/triplet_utils.dir/src/bundle.cpp.o"
+  "/home/er/Documents/d_dev/structureless/propcov/relative-motions/src/cov_in_motions.cpp" "/home/er/Documents/d_dev/structureless/propcov/relative-motions/build/CMakeFiles/triplet_utils.dir/src/cov_in_motions.cpp.o"
+  "/home/er/Documents/d_dev/structureless/propcov/relative-motions/src/triplet_utils.cpp" "/home/er/Documents/d_dev/structureless/propcov/relative-motions/build/CMakeFiles/triplet_utils.dir/src/triplet_utils.cpp.o"
+  "/home/er/Documents/d_dev/structureless/propcov/relative-motions/src/triplet_utils_main.cpp" "/home/er/Documents/d_dev/structureless/propcov/relative-motions/build/CMakeFiles/triplet_utils.dir/src/triplet_utils_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/triplet_utils.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triplet_utils.dir/src/bundle.cpp.o"
+  "CMakeFiles/triplet_utils.dir/src/cov_in_motions.cpp.o"
+  "CMakeFiles/triplet_utils.dir/src/triplet_utils.cpp.o"
+  "CMakeFiles/triplet_utils.dir/src/triplet_utils_main.cpp.o"
+  "triplet_utils"
+  "triplet_utils.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/triplet_utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
